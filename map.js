04ffdbf377f66517aace60c6539416bd79097ef9
chicken-finger-backup <aliyahanderson98@gmail.com>
2025-10-09 21:51:09 -0500

    CHange to JavaScript

--- map.js
+++ map.js
@@ -6,3 +6,14 @@ L.tileLayer(
     attribution: '&copy; ' + mapLink + ' Contributors',
     maxZoom: 18,
     }).addTo(map);
+    var polygon = turf.polygon([
+  [
+    [125, -15],
+    [113, -22],
+    [154, -27],
+    [144, -15],
+    [125, -15],
+  ],
+]);
+
+var area = turf.area(polygon);
