b49b7f18595e220c4af0a550f98db49797ca7ae1
Alison <arae070503@gmail.com>
2025-10-15 22:37:33 -0500

    alison's function code added

--- map.js
+++ map.js
@@ -31,3 +31,14 @@ L.tileLayer(
 
     var area = turf.area(polygon);
   L.geoJSON(polygon).addTo(map);
+
+  var pointsCollection = turf.points([
+    [-97.942129, 29.887412],
+    [-97.934683, 29.888101],
+    [-97.941099, 29.891235],
+    [-97.947461, 29.890166]
+  ])
+  L.geoJson(pointsCollection).addTo(map);
+  var center = turf.center(pointsCollection);
+
+  L.geoJSON(center).addTo(map);
