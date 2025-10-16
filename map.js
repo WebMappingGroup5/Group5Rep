862f88774ee5f162b9e093dde11db423f19cb47c
Alison <arae070503@gmail.com>
2025-10-15 21:49:29 -0500

    trial 1.2 (area of points)

--- map.js
+++ map.js
@@ -17,3 +17,12 @@ L.tileLayer(
     var center = turf.center(pointsCollection);
 
     L.geoJSON(center).addTo(map);
+    var area = turf.polygon([[
+      [-97.942129, 29.887412],
+      [-97.934683, 29.888101],
+      [-97.941099, 29.891235],
+      [-97.947461, 29.890166],
+      [-97.942129, 29.887412]
+    ]])
+
+    L.geoJSON(polygon).addTo(map);
